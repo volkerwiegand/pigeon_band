c5cd27325210d34475353cfd147fdc0ec1dce8b9
Volker Wiegand <volker.wiegand@cvw.de>
2015-10-31 07:20:37 +0100

    added year validation

--- lib/pigeon_band.rb
+++ lib/pigeon_band.rb
@@ -8,13 +8,13 @@ module PigeonBand
     list = band.split('-')
     case list[0]
       when 'B'
-        year = list[1].to_i
+        year = checked_year(band, list[1])
         sequ = list[2].to_i
         band = sprintf("B-%02d-%07d", year % 100, sequ)
         slug = band
       when 'DV'
         club = list[1].to_i
-        year = list[2].to_i
+        year = checked_year(band, list[2])
         sequ = list[3].to_i
         band = sprintf("DV-0%d-%02d-%d", club, year % 100, sequ)
         slug = sprintf("DV-%05d-%02d-%04d", club, year % 100, sequ)
@@ -24,9 +24,19 @@ module PigeonBand
         band = sprintf("NL-%02d-%07d", year % 100, sequ)
         slug = band
       else
-        raise "Please add more formats on https://github.com/volkerwiegand/pigeon_band"
+        raise "Please add more formats at https://github.com/volkerwiegand/pigeon_band"
     end
     [band, slug, year]
+  end
+
+private
+
+  def formatted_year(band, year)
+    msg = "invalid year '#{year}' in pigeon band '#{band}'"
+    raise msg unless year.match(/^\d/)
+    return 2000 + year if year in  0..59
+    return 1900 + year if year in 60..99
+    raise msg
   end
 end
 
